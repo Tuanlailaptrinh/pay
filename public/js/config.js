925d037d4a81e96351eb3b3bac7ed5587dfac281
Tuanlailaptrinh <87818411+Tuanlailaptrinh@users.noreply.github.com>
2022-03-17 19:52:56 +0700

    Add files via upload

---
+++ public/js/config.js
@@ -0,0 +1,42 @@
+const WEB = {
+  title: "PAYMENT",
+  favicon: "favicon.png",
+};
+
+const USER = {
+  username: "Nguyễn Huỳnh Thái Tuấn",
+  blueTick: false,
+  avatar: "logonhan.png",
+  skills: [
+      "Social Media",
+      "DEVELOPER"
+  ],
+  banks: [
+      {
+          name: "VPBANK",
+          logo: "vp.png",
+          number: "530062007",
+          owner: "NGUYEN HUYNH THAI TUAN"
+      },
+      {
+          name: "MB Bank ",
+          logo: "mb.png",
+          number: "30620079999",
+          owner: "NGUYEN HUYNH THAI TUAN"
+      },
+      {
+          name: "VIETCOMBANK",
+          logo: "vcb.jpg",
+          number: "9383556654",
+          owner: "HUYNH THI HUE LINH",
+      },
+      {
+         name: "Momo",
+         logo: "momo.jpg",
+         number: "0383556654",
+         owner: "HUYNH THI HUE LINH",
+         fastTransfer: true
+    },
+
+  ]
+};
